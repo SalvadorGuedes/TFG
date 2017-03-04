e17ac98656ee7f60c7b0596d99127849f66ac139
SalvadorGuedes <sgs.1995@hotmail.com>
2017-03-04 13:01:44 +0000

    Comienzo explorador de archivos
    Creación de la Base de datos y configuración.
    Creación de la entidad y formulario.
    Pendiente de comprobar y terminar.

---
+++ P_NN2U/src/generalBundle/Entity/Images.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace generalBundle\Entity;
+
+/**
+ * Images
+ */
+class Images
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $extension;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Images
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set extension
+     *
+     * @param string $extension
+     *
+     * @return Images
+     */
+    public function setExtension($extension)
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * Get extension
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+}
+
---
+++ P_NN2U/src/generalBundle/Form/ImagesType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace generalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ImagesType extends AbstractType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+//        $builder->add('name')->add('extension')        ;
+        $builder->add('image', FileType::class, array(
+            "label" => "Imagen:",
+            "attr" => array("class" => "form-control")
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'generalBundle\Entity\Images'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix() {
+        return 'generalbundle_images';
+    }
+
+}
